926c543b5619134656001f5c2bb1552372b21526
Tom Wright <thomasjameswright@hotmail.co.uk>
2018-11-21 16:29:19 +0000

    created basic registrar class

---
+++ Classes/Accounts/Registrar.java
@@ -0,0 +1,11 @@
+/* Registrar.java subclass for account */
+package Accounts;
+public class Registrar extends Account {
+
+	public Registrar(String userID, String name, String password) {
+		super(userID, name, password);
+	}
+	
+	// need database stuff working first 
+	
+}
