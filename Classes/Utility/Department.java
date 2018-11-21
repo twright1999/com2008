3e8020edab42d4093655f4ba5f7cb008c782364c
Rokas <34490649+RokasBagdonas@users.noreply.github.com>
2018-11-21 17:15:44 +0000

    Department class is completed(?), started working Module class.

--- Classes/Utility/Department.java
+++ Classes/Utility/Department.java
@@ -1,5 +1,25 @@
 package Utility;
-
+/**
+ * 
+ * @author Rokas Bagdonas
+ * 
+ */
 public class Department {
-
+	
+	private String depId; //Three letter code (Like COM)
+	private String name;  //Full name (like Computer Science)
+	private Degree[] degrees; //Degrees available in this department
+	
+	public Department(String depId, String name, Degree[] degrees) {
+		this.depId = depId;
+		this.name = name;
+		this.degrees = degrees;
+	}
+	
+	public String getDepId() { return depId ; }
+	public String getName() { return name ; }
+	public Degree[] getDegree() { return degrees; }
+	
+	
+	
 }
--- Classes/Utility/Module.java
+++ Classes/Utility/Module.java
@@ -1,5 +1,29 @@
 package Utility;
-
+/**
+ * 
+ * @author Rokas
+ *
+ */
 public class Module {
-
+	enum Period { Autum, Spring, Full_Year };
+	
+	private String moduleId; //department's code  + 4 digit number
+	private String name; //Full name (Functional programming)
+	private int credits;
+	private Enum period; 
+	private Boolean obligatory;
+	private String degId;
+	private Department[] departments;
+	
+	public Module(String moduleId, String name, int credits, Period period,
+			Boolean obligatory, String degId, Department[] departments) {
+		this.moduleId = moduleId;
+		this.name = name;
+		this.credits = credits;
+		this.period = period;
+		this.obligatory = obligatory;
+		this.degId = degId;
+		this.departments = departments;
+	}
+	
 }
