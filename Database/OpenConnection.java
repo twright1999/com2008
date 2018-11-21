6c0c85b068539c2c671bf7cf9536247d8cfc8872
Tom Wright <thomasjameswright@hotmail.co.uk>
2018-11-21 20:09:23 +0000

    changed open connection to be in the 'new style'

--- Database/OpenConnection.java
+++ Database/OpenConnection.java
@@ -2,19 +2,12 @@ package Database;
 import java.sql.*;
 public class OpenConnection {
 	public static void main(String[] arg) throws SQLException {
-		Connection con = null; // a Connection object
-		try {
-			con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team020", "team020", "aa429b86");
-					
-				// use the open connection
-				// for several queries
-					
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team020", "team020", "aa429b86")) {
+			 // use the open connection
+			 // for one or more queries
 		}
 		catch (SQLException ex) {
 			ex.printStackTrace();
-		}
-		finally {
-			if (con != null) con.close();
 		}
 	}
 }
