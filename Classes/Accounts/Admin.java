845ab34d3fb8c77a5f1af4f589bf6d2d08cc5a77
cameronmcwilliam <cameronmcwilliam@macbook-pro-2.lan>
2018-11-22 13:04:55 +0000

    Admin account first version pushed

---
+++ Classes/Accounts/Admin.java
@@ -0,0 +1,27 @@
+package Accounts;
+
+import java.sql.*;
+public class Admin extends Account {
+
+	public Admin(String userID, String name, String password) {
+		super(userID, name, password);
+		
+	}
+
+	private void addAcount(String name, String password) throws SQLException {
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team020", "team020", "aa429b86");
+		Statement stmt = con.createStatement() ;
+		Statement s=con.createStatement();
+		s.executeUpdate("INSERT INTO `Account`(name,password) VALUE ('"+name+"','"+password+"')");
+		con.close();
+		
+	}
+	
+	private void removeAccount(Account account) {
+		
+		
+		
+	}
+	
+}
