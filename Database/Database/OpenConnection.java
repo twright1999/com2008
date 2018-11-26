a0532ebc629e8074ef1350ca03f3670087cbf9a3
Tom Wright <thomasjameswright@hotmail.co.uk>
2018-11-26 14:31:51 +0000

    changed some tests

--- Database/Database/OpenConnection.java
+++ Database/Database/OpenConnection.java
@@ -7,13 +7,6 @@ public class OpenConnection {
 		try (Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team020", "team020", "aa429b86")) {
 			stmt = con.createStatement();
 
-// code thats been executed, don't run again
-// this class can be used to create tables
-			/*
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Account("+
-							   "accountID INT (6) NOT NULL PRIMARY KEY UNIQUE,"+
-							   "name text NOT NULL,"+
-							   "password text NOT NULL)");*/
 			//stmt.executeUpdate("INSERT INTO Account VALUES (122, 'John', '123')");
 			//stmt.executeQuery("SELECT * FROM Account WHERE accountID = 122");
 			/*
@@ -22,16 +15,31 @@ public class OpenConnection {
 							+ "=?");
 			values.setString(1, "122");*/
 			
-			ResultSet res = stmt.executeQuery("SELECT * FROM Account WHERE accountID = 122");
+//			stmt.executeUpdate("INSERT INTO Account VALUES (1, 'Tom', '123', 'S')");
+//		    stmt.executeUpdate("INSERT INTO Student VALUES (123456789, 'twright6@sheffield.ac.uk', 'richard', '1')");
+			
+			ResultSet res = stmt.executeQuery("SELECT * FROM Account WHERE userID = 1");
 			while (res.next()) {
 				String name = res.getString("name");
 				System.out.print("RESULT NAME: " + name);
-				String accountID = res.getString("accountID");
+				String accountID = res.getString("userID");
 				System.out.print(" accID: " + accountID);
 				String password = res.getString("password");
 				System.out.println(" pass: " + password);
-				Account acc = new Account(accountID, name, password);
-				System.out.println(acc.getName());
+				String permission = res.getString("permission");
+				System.out.println(" permission: " + permission);
+			}
+			
+			ResultSet res2 = stmt.executeQuery("SELECT * FROM Student WHERE userID = 1");
+			while (res2.next()) {
+				String regNumber = res2.getString("regNumber");
+				System.out.print("RESULT regNumber: " + regNumber);
+				String email = res2.getString("email");
+				System.out.print(" email: " + email);
+				String tutor = res2.getString("tutor");
+				System.out.println(" tutor: " + tutor);
+				String userID = res2.getString("userID");
+				System.out.println(" userID: " + userID);
 			}
 			
 			stmt.close();
