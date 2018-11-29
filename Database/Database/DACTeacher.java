9583b8d0ed696c9e9bd2678e5fdb72045f3964b4
Tom Wright <thomasjameswright@hotmail.co.uk>
2018-11-29 16:04:54 +0000

    changed calcPeriod to take individual pass marks into consideration

--- Database/Database/DACTeacher.java
+++ Database/Database/DACTeacher.java
@@ -45,18 +45,29 @@ public class DACTeacher extends DAC {
 		
 		float totalPercent = 0;
 		int totalGrades = 0;
+		boolean passedEveryModule = true;
+		
 		while (gradeQuery.next()) {
 			totalPercent += gradeQuery.getFloat("gradePercent");
-				totalGrades += 1;
-			}
+			totalGrades += 1;
+			if (gradeQuery.getString("level") == "P");
+			else if (Integer.parseInt(gradeQuery.getString("level")) == 4 && gradeQuery.getFloat("gradePercent") < 50)
+				passedEveryModule = false;
+			else if (Integer.parseInt(gradeQuery.getString("level")) < 4 && gradeQuery.getFloat("gradePercent") < 40)
+				passedEveryModule = false;				
+		}
 			
 			closeConnection();
 			float average = totalPercent/totalGrades;
-			if (average >= 40.0)
+			if (average >= 40.0 && passedEveryModule)
 				return true;
 			else
 				return false;
 		}
+	
+	public static void nextPeriod() {
+		
+	}
 	
 	//for testing
 	public static void main(String[] arg) throws SQLException {
