0170f4699024fff221c222157726cc3d480db07e
Tom Wright <thomasjameswright@hotmail.co.uk>
2018-11-29 21:49:48 +0000

    created teacher class

---
+++ Classes/Accounts/Teacher.java
@@ -0,0 +1,30 @@
+package Accounts;
+import java.sql.SQLException;
+import Database.DACTeacher;
+
+public class Teacher extends Account {
+
+	public Teacher(int userID, String name, String password, char permission) {
+		super(userID, name, password, permission);
+	}
+	
+	private void addGrade(float gradePercent, String modID, int regNumber) throws SQLException {
+		DACTeacher.addGrade(gradePercent, modID, regNumber);
+	}
+	
+	private void removeGrade(int gradeID) throws SQLException {
+		DACTeacher.removeGrade(gradeID);
+	}
+	
+	private boolean calcPeriod(int regNumber, int periodID) throws SQLException {
+		return DACTeacher.calcPeriod(regNumber, periodID);	
+	}
+	
+	private String calcDegree(int regNumber) throws SQLException {
+		return DACTeacher.calcDegree(regNumber);
+	}
+	
+	private String getStudentStatus(int regNumber) throws SQLException {
+		return DACTeacher.getStudentStatus(regNumber);
+	}
+}
